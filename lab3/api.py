75455a738a034cac557e152e42d5a013121c35f4
unknown <nguyenhaoquang2004@gmail.com>
2025-06-11 15:35:46 +0700

    lab4

--- lab3/api.py
+++ lab3/api.py
@@ -1,7 +1,36 @@
 from flask import Flask, request, jsonify
 from cipher.rsa import RSACipher
-
+from cipher.ecc import ECCCipher
 app = Flask(__name__)
+
+
+# Thêm đoạn này vào cuối hàm main
+# ECC CIPHER ALGORITHM
+ecc_cipher = ECCCipher()
+
+@app.route('/api/ecc/generate_keys', methods=['GET'])
+def ecc_generate_keys():
+    ecc_cipher.generate_keys()
+    return jsonify({'message': 'Keys generated successfully'})
+
+@app.route('/api/ecc/sign', methods=['POST'])
+def ecc_sign_message():
+    data = request.json
+    message = data['message']
+    private_key, _ = ecc_cipher.load_keys()
+    signature = ecc_cipher.sign(message, private_key)
+    signature_hex = signature.hex()
+    return jsonify({'signature': signature_hex})
+
+@app.route('/api/ecc/verify', methods=['POST'])
+def ecc_verify_signature():
+    data = request.json
+    message = data['message']
+    signature = data['signature']
+    public_key, _ = ecc_cipher.load_keys()
+    signature_bytes = bytes.fromhex(signature)
+    is_verified = ecc_cipher.verify(message, signature_bytes, public_key)
+    return jsonify({'is_verified': is_verified})
 
 #RSA CIPHER ALGORITHM
 rsa_cipher = RSACipher()
